2f33d313dfaad28711061675cf7f3b46794c1321
pengchengZenjoy <cheng.peng@zenjoy.net>
2019-06-06 18:26:54 +0800

    port

--- examples/main.lua
+++ examples/main.lua
@@ -13,10 +13,10 @@ skynet.start(function()
 	skynet.newservice("simpledb")
 	local watchdog = skynet.newservice("watchdog")
 	skynet.call(watchdog, "lua", "start", {
-		port = 8888,
+		port = 8877,
 		maxclient = max_client,
 		nodelay = true,
 	})
-	skynet.error("Watchdog listen on", 8888)
+	skynet.error("Watchdog listen on", 8877)
 	skynet.exit()
 end)
